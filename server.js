4fe52ec1eeea3d3f33a963ce6deafb1e984cd8eb
Julian Belmartino <belmartinojulian@gmail.com>
2022-11-10 21:35:22 -0300

    b

--- server.js
+++ server.js
@@ -21,11 +21,34 @@ const connectionDB = require('./config.js')
 connectionDB()
 const connectionDBs = require('./config.js')
 connectionDBs()
-
-
+const nodemailer = require("nodemailer");
+const { createTransport } = require('nodemailer')
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+const http = require("http")
+/*
 httpServer.listen(8080, () =>{
   console.log(httpServer.address().port)
 })
+*/
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`)
+  for (let i = 0; i < numCPUs; i++) {
+      cluster.fork()
+  }
+  cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`)
+  })
+} else {
+  http.createServer((req, res) => {
+      res.writeHead(200)
+      res.end('hello world')
+  }).listen(8080)
+  
+  console.log(`Worker ${process.pid} started`)
+}
+
+
 httpServer.on('error', err => console.log(err))
 
   app.set('views','./views')
@@ -60,3 +83,29 @@ app.use('/', routerLogin)
 app.use('/', routerProductos)
 
 
+const TEST_MAIL = 'belmartinojulian@gmail.com'
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  port: 587,
+  auth: {
+      user: TEST_MAIL,
+      pass: 'thhojhubxxqhkpvs'
+  }
+});
+
+const mailOptions = {
+  from:'Servidor de Node',
+  to:TEST_MAIL,
+  subject:'Mail de prueba',
+  html:'<h1>Hola</h1>'
+}
+;(async () => {
+try {
+  const info = await transporter.sendMail(mailOptions)
+  console.log(info)
+} catch (error) {
+  console.log(error)
+}
+})()
+
+//thhojhubxxqhkpvs
